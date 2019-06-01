2e5d1cd46cb2d1bbb9983aa68eba8b608725356a
GradedJestRisk <graded.jest.risk@gmail.com>
2019-06-01 16:04:43 +0200

    Battleship - Add view and test code

--- battleship/battleship.js
+++ battleship/battleship.js
@@ -1,1 +1,31 @@
 console.log("hello, world, battleship there");
+
+var view = {
+
+    displayMessage: function (msg){
+        var messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+
+    displayHit : function (location){
+
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+    },
+
+    displayMiss : function (location){
+
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
+    }
+
+}
+
+view.displayMessage("Anyone here ?");
+
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
