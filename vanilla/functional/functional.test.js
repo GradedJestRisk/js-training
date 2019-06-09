475021301caf51c12b0fdefeca0cd9ad0892d0eb
GradedJestRisk <graded.jest.risk@gmail.com>
2019-06-09 17:05:10 +0200

    Vanilla - Functional - Added map/reduce/filter

--- vanilla/functional/functional.test.js
+++ vanilla/functional/functional.test.js
@@ -5,12 +5,13 @@ describe('Functional tests ', function() {
 
 		test('map applies an operator on each element', () => {
 
-            anArray     =  [1, 2, 3];
-            squaredArray = [1, 5, 9];
-			expect(
-                anArray.map(function(elem){ return elem*elem })
-                ).toBe(squaredArray);
-		
+			anArray     =  [1, 2, 3];
+			squaredArray = [1, 4, 9];
+
+			mappedArray =  anArray.map(function(elem){ return elem * elem });
+			
+			expect( mappedArray).toStrictEqual(squaredArray);
+
 		});
 
 	
@@ -21,10 +22,30 @@ describe('Functional tests ', function() {
 
 		test('reduce applies an operator on all elements ', () => {
 
+			anArray    = [1,  2,  3];
+			arrayTotal =  1 + 2 + 3;
+
+			expect( 
+				anArray.reduce(function(total, elem){ return elem + total }, 0)
+			).toBe(arrayTotal);
 
 		});
 
 	});
 
+	describe('Filter features', function() {
+
+		test('filter keep all elements matching a condition ', () => {
+
+			anArray             = [1, 2, 3, 4];
+			arrayWithEvenNumber = [   2,    4];
+
+			filteredArray =  anArray.filter(function(elem){return elem % 2 == 0; });
+				
+			expect(filteredArray).toStrictEqual(arrayWithEvenNumber);
+
+		});
+
+	});
 
 });
