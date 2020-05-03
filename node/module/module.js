7ae07d57a15bb2d372f8532133084e34cbf66e62
GradedJestRisk <graded.jest.risk@gmail.com>
2020-05-03 10:13:08 +0200

    [Node] Module - Add pass-by-reference mutation

--- node/module/module.js
+++ node/module/module.js
@@ -1,6 +1,10 @@
 let mutableString = "initialMutableState";
 const immutableString = "immutableState";
 
+const object = { string: "initialMutableState" }
+
+
 let counter = 0;
-const nextInteger = function (){ return(++counter); };
-module.exports = { mutableString, immutableString, nextInteger };
+const nextInteger = function (){ return ++counter; };
+
+module.exports = { mutableString, immutableString, object, nextInteger };
--- node/module/module.test.js
+++ node/module/module.test.js
@@ -4,10 +4,12 @@ const expect = require('chai').expect;
 const firstImport = require('./module.js');
 let mutableStringFirstImport = firstImport.mutableString
 const immutableStringFirstImport = firstImport.immutableString;
+const mutableObjectFirstImport = firstImport.object;
 const nextIntegerFirstImport = firstImport.nextInteger;
 
 const secondImport = require('./module.js');
 let mutableStringSecondImport = secondImport.mutableString
+const mutableObjectSecondImport = secondImport.object;
 const nextIntegerSecondImport = secondImport.nextInteger;
 
 chai.should();
@@ -36,9 +38,14 @@ describe('immutableData', () => {
 
 describe('mutableData', () => {
 
-    it('state is not shared - require returns a reference', () => {
+    it('state is not shared, when require returns a value (primitive type), not a reference', () => {
         mutableStringFirstImport = "mutatedStateOnFirstImport";
         mutableStringSecondImport.should.equal("initialMutableState");
+    });
+
+    it('state is shared, when require returns a reference (object)', () => {
+        mutableObjectFirstImport.string = "mutatedStateOnFirstImport";
+        mutableObjectSecondImport.string.should.equal(mutableObjectFirstImport.string);
     });
 
     it('private state is shared', () => {
