d8da0dff585ce98fef3c0d7d940a21ee4b656c9f
GradedJestRisk <graded.jest.risk@gmail.com>
2020-05-24 15:15:24 +0200

    [node] orm-less: add transaction in test script

--- node/persistence/db/orm-less/src/repositories/recipe.js
+++ node/persistence/db/orm-less/src/repositories/recipe.js
@@ -1,8 +1,32 @@
 const knex = require('../../knex/knex.js');
+const tableName = 'recipe';
+// knex.on('query', console.log)
 
 const getAll = async function()  {
-    const recipes = await  knex.from('recipe').select('name', 'serving');
+    const recipes = await  knex.from(tableName).select('name', 'serving');
     return recipes;
 }
 
-module.exports = { getAll }
+const get = async function({ minServing })  {
+    const recipes = await  knex.from(tableName).select('name', 'serving').where('serving', '>=', minServing);
+    return recipes;
+}
+
+const getByName = async function(name)  {
+    const recipes = await  knex.from(tableName).select('name', 'serving').where('name');
+    return recipes;
+}
+
+const count = async function()  {
+    return Number( (await knex.from(tableName).count('name').first()).count);
+}
+
+const create = async function(recipe)  {
+   await knex(tableName).insert([recipe]);
+}
+
+const removeAll = async function () {
+    await knex(tableName).del();
+};
+
+module.exports = { getAll, get, create, getByName, removeAll, count };
--- node/persistence/db/orm-less/test/test.js
+++ node/persistence/db/orm-less/test/test.js
@@ -1,12 +1,42 @@
 const knex = require('../knex/knex.js');
 const chai = require('chai');
+const recipeRepository = require('../src/repositories/recipe');
 chai.should();
+
+const tableName = 'recipe';
 
 describe('test', async () => {
 
-    it('raw should run plain SQL', async ()=> {
+    beforeEach(async () => {
+        await recipeRepository.removeAll();
+    });
+
+    it('raw should run plain SQL', async () => {
         const currentDatabaseQuery = 'SELECT * FROM current_database()';
         const currentDatabase = (await knex.raw(currentDatabaseQuery)).rows[0].current_database;
         currentDatabase.should.eq('cooking_db');
     })
+
+    it('error in transaction should prevent data insertion', async () => {
+
+        const recipe = {
+            name: 'caramelized-garlic-tart',
+            serving: 4,
+            source: 'https://www.theguardian.com/lifeandstyle/2008/mar/01/foodanddrink.shopping1'
+        };
+
+        try {
+            await knex.transaction(async trx => {
+                await trx(tableName).insert(recipe);
+                await trx('unknownTable').insert(recipe);
+            })
+        } catch (error) {
+            //console.error("Error raised:" + error);
+            const count = await recipeRepository.count();
+            count.should.equal(0);
+        }
+
+    })
+
+
 });
