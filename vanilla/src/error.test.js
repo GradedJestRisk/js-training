643cad148f05103c94702fc9328c39bea838ed1a
GradedJestRisk <graded.jest.risk@gmail.com>
2020-05-25 09:05:53 +0200

    [vanilla] Error - Add try/catch/finally

--- vanilla/src/error.test.js
+++ vanilla/src/error.test.js
@@ -1,14 +1,56 @@
-describe('throw', () => {
+describe('error', () => {
 
-  test('should throw', () => {
+    test('should throw', () => {
 
-    const myThrowingFunction = function(){
-      throw new Error("Message");
-    };
+        const myThrowingFunction = function () {
+            throw new Error("Message");
+        };
 
-    expect( () => {myThrowingFunction()}).toThrow(Error("Message"));
+        expect(() => {
+            myThrowingFunction()
+        }).toThrow(Error("Message"));
 
-  });
+    });
+
+    describe('try/catch', () => {
+
+        test('return in try exit the function', () => {
+            const RETURN_TRY = 0;
+            const RETURN_BODY= 1;
+
+            const aFunction = function () {
+                try {
+                    return RETURN_TRY
+                } catch (error) {
+                }
+                return RETURN_BODY;
+            };
+
+            const value = aFunction();
+
+            expect(value).toBe(RETURN_TRY);
+
+        });
+
+        test('return in finally exit the function', () => {
+            const RETURN_TRY = 0;
+            const RETURN_FINALLY = 1;
+
+            const aFunction = function () {
+                try {
+                    return RETURN_TRY
+                } catch (error) {
+                } finally {
+                    return RETURN_FINALLY;
+                }
+            };
+
+            const value = aFunction();
+
+            expect(value).toBe(RETURN_FINALLY);
+
+        });
+    });
 
 
 });
