12b0540a82ef5a641521ced74763ade52f5d9636
GradedJestRisk <graded.jest.risk@gmail.com>
2020-05-26 13:56:19 +0200

    [vanilla] Error - Add await in try

--- vanilla/src/error.test.js
+++ vanilla/src/error.test.js
@@ -50,6 +50,30 @@ describe('error', () => {
             expect(value).toBe(RETURN_FINALLY);
 
         });
+
+        test('return in try using await exit the function',  async () => {
+            const RETURN_TRY = 0;
+            const RETURN_BODY = 1;
+
+            const sleep = function sleep(ms) {
+                return new Promise(resolve => setTimeout(resolve, ms));
+            };
+
+            const aFunction = async function () {
+                try {
+                    await sleep(1000);
+                    return RETURN_TRY
+                } catch (error) {}
+
+                return RETURN_BODY;
+
+                };
+
+            const value = await aFunction();
+
+            expect(value).toBe(RETURN_TRY);
+
+        });
     });
 
 
