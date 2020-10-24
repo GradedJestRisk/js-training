17fae5b763bc1aaa34c023985b064526a09a9dce
GradedJestRisk <graded.jest.risk@gmail.com>
2020-10-24 16:03:30 +0200

    [code-quality][autofix] create

---
+++ node/code-quality/format-autofix/src/foo.js
@@ -0,0 +1,9 @@
+// previously committed chunk - doesn't comply with prettier rules
+const foo = "bar";
+
+if (foo === 'bar') {
+  console.log('foo!');
+}
+
+// added chunk - doesn't comply with prettier rules
+console.log("foobar!");
