364769974d1033626112784927df43d4a92ef84a
GradedJestRisk <graded.jest.risk@gmail.com>
2020-11-02 14:00:56 +0100

    [node][scheduling] Add weekDays

--- node/scheduling/schedule.js
+++ node/scheduling/schedule.js
@@ -16,23 +16,33 @@ const parisTimezone = 'Europe/Paris';
 // Pattern doc => http://crontab.org/ or https://crontab.guru
 // Note: this module has second granularity, whereas unix cron doesn't (so only 5 options)
 
-const eachSecond    = ' * * * * * *';
-const each5Seconds  = ' */5 * * * * *';
-const eachSecond5   = '5 * * * * *';
+const eachSecond    =             '* * * * * *';
+const each5Seconds  =             '*/5 * * * * *';
+const eachSecond5   =             '5 * * * * *';
+const eachSecondOnWeekDays =      '* * * 1,2,3,4,5 * *'
+const eachMinuteThreeOnWeekDays = '* 3 * 1,2,3,4,5 * *'
+const each14h35OnWeekDays =       '0 35 14 1,2,3,4,5 * *'
 
 const timeFormat = 'HH:mm:ss';
 
 // API here => https://github.com/kelektiv/node-cron#api
 const cron1 = new CronJob(eachSecond, function () {
     const now = dayjs().format(timeFormat);
-    console.log(now + ': I am cron1, scheduled each second - ' + eachSecond)
+    console.log(now + ': I am cron1, scheduled each second - - pattern is:' + eachSecond)
 }, null, true, parisTimezone);
 
 const cron2 = new CronJob(each5Seconds, function () {
-    console.log('I am cron2, scheduled each 5s - ' + each5Seconds)
+    console.log('I am cron2, scheduled each 5s - pattern is:' + each5Seconds)
 }, null, true, parisTimezone);
 
 const cron3 = new CronJob(eachSecond5, function () {
-    console.log('I am cron3, scheduled each each ** h ** min 05s - ' + eachSecond5 )
+    console.log('I am cron3, scheduled each ** h ** min 05s - pattern is: ' + eachSecond5 )
 }, null, true, parisTimezone);
 
+const cron4 = new CronJob(eachSecondOnWeekDays, function () {
+    console.log('I am cron4, scheduled each second on week days - pattern is: ' + eachSecondOnWeekDays )
+}, null, true, parisTimezone);
+
+const cron5 = new CronJob(each14h35OnWeekDays, function () {
+    console.log('I am cron5, scheduled on 14h35m00s on week days - pattern is: ' + each14h35OnWeekDays )
+}, null, true, parisTimezone);
