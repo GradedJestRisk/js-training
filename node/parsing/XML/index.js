824a9e5a00f1ba1a8accb69ad1f9f72131e940a3
GradedJestRisk <graded.jest.risk@gmail.com>
2020-11-03 20:20:24 +0100

    [node][parsing][xml] use faker

--- node/parsing/XML/index.js
+++ node/parsing/XML/index.js
@@ -1,44 +1,68 @@
 const js2xmlparser = require("js2xmlparser");
- 
-const person = {
-    "firstName": "John",
-    "lastName": "Smith",
-    "dateOfBirth": new Date(1964, 7, 26),
-    "address": {
-        "@": {
-            "type": "home"
-        },
-        "streetAddress": "3212 22nd St",
-        "city": "Chicago",
-        "state": "Illinois",
-        "zip": 10000
-    },
-    "phone": [
-        {
+const faker = require('faker');
+
+const basicUse = function () {
+    const person = {
+        "firstName": "John",
+        "lastName": "Smith",
+        "dateOfBirth": new Date(1964, 7, 26),
+        "address": {
             "@": {
                 "type": "home"
             },
-            "#": "123-555-4567"
+            "streetAddress": "3212 22nd St",
+            "city": "Chicago",
+            "state": "Illinois",
+            "zip": 10000
         },
-        {
-            "@": {
-                "type": "cell"
+        "phone": [
+            {
+                "@": {
+                    "type": "home"
+                },
+                "#": "123-555-4567"
             },
-            "#": "890-555-1234"
-        },
-        {
-            "@": {
-                "type": "work"
+            {
+                "@": {
+                    "type": "cell"
+                },
+                "#": "890-555-1234"
             },
-            "#": "567-555-8901"
+            {
+                "@": {
+                    "type": "work"
+                },
+                "#": "567-555-8901"
+            }
+        ],
+        "email": "john@smith.com"
+    };
+    const rootXMLNode = 'person';
+    const personXML = js2xmlparser.parse(rootXMLNode, person);
+
+    console.log(person);
+    console.log(personXML);
+
+}
+
+const useWithFaker = function() {
+
+    const users = [];
+    const USER_COUNT = 5;
+
+    for (let i = 0; i < USER_COUNT; i++) {
+
+        const user = {
+            name: faker.name.findName(),
+            email: faker.internet.email()
         }
-    ],
-    "email": "john@smith.com"
-};
- 
+        users.push(user);
+    }
 
-const rootXMLNode = 'person';
-const personXML = js2xmlparser.parse(rootXMLNode, person);
+    const rootXMLNode = 'persons';
+    const usersRootNode = { 'user' : users };
+    console.log(js2xmlparser.parse(rootXMLNode, usersRootNode));
+}
 
-console.log(person);
-console.log(personXML);
+basicUse();
+useWithFaker();
