38795b738c0d0bb73d5f69fe17dcdefcdf37a154
GradedJestRisk <graded.jest.risk@gmail.com>
2021-03-16 18:12:57 +0100

    [vanilla][functional] add map on await

--- vanilla/src/functional/functional.test.js
+++ vanilla/src/functional/functional.test.js
@@ -1,147 +1,170 @@
+describe('Functional tests ', function () {
 
-describe('Functional tests ', function() {
+   describe('IIFE', function () {
 
-	describe('IIFE', function() {
+      describe('arrow function style ', function () {
 
-		describe('arrow function style ', function() {
+         test('shortest form, no parameter, expression', () => {
 
-			test('shortest form, no parameter, expression', () => {
+            const expectedWord = 'foo';
 
-				const expectedWord = 'foo';
+            const actualWord = (() => 'foo')();
 
-				const actualWord = (() => 'foo')();
+            expect(actualWord).toStrictEqual(expectedWord);
 
-				expect(actualWord).toStrictEqual(expectedWord);
+         });
 
-			});
+         test('use statements, a parameter, expression', () => {
 
-			test('use statements, a parameter, expression', () => {
+            const name = 'foo';
+            const expectedLength = 3;
 
-				const name = 'foo';
-				const expectedLength = 3;
+            const actualLength = (nameParameter => nameParameter.length)(name);
 
-				const actualLength = ( nameParameter => nameParameter.length )(name);
+            expect(
+               actualLength)
+               .toStrictEqual(expectedLength);
+         });
 
-				expect(
-					actualLength)
-					.toStrictEqual(expectedLength);
-			});
+         test('use statements, several parameters, statement', () => {
 
-			test('use statements, several parameter, statement', () => {
+            const thisString = "Hello", thatString = ", world !";
+            const expectedString = "Hello, world !"
 
-				const thisString = "Hello", thatString = ", world !";
-				const expectedString = "Hello, world !"
+            const actualString =
+               ((aString, anotherString) => {
+                  return (aString + anotherString);
+               })
+               (thisString, thatString);
 
-				const actualString =
-							((aString, anotherString) => { return(aString + anotherString); } )
-							(thisString, thatString);
+            expect(actualString).toStrictEqual(expectedString);
+         });
 
-				expect(
-					actualString)
-					.toStrictEqual(actualString);
-			});
+         test('composed with a regular function ', () => {
 
-			test('composed with a regular function ', () => {
+            const materials = ['Hydrogen', 'Helium', 'Lithium'];
+            const expectedLength = [8, 6, 7];
 
-				const materials = [ 'Hydrogen', 'Helium', 'Lithium'];
-				const expectedLength = [ 8, 6, 7];
+            const actualLength = materials.map(material => material.length);
 
-				actualLength =  materials.map(material => material.length);
+            expect(actualLength).toStrictEqual(expectedLength);
 
-				expect(actualLength).toStrictEqual(expectedLength);
+         });
 
-			});
+      });
 
-		});
+      describe('regular function style ', function () {
 
-		describe('regular function style ', function() {
+         test('without parameters, use statements, delayed invocation (not IIFE) ', () => {
 
-			test('without parameters, use statements, delayed invocation (not IIFE) ', () => {
+            const generateBoolean = () => {
+               const flag = Math.random() > 0.5
+               return flag ? true : false
+            }
 
-				const booleanValues = [true, false];
+            expect(generateBoolean()).toBeBoolean();
 
-				const generateBoolean = () => {
-					const flag = Math.random() > 0.5
-					return flag ? 'true' : 'false'
-				}
+         });
 
-				expect(generateBoolean().toBeBoolean);
+         test('with parameter, use statements', () => {
 
-			});
+            const name = 'foo';
 
-			test('with parameter, use statements', () => {
+            const expectedGreeting = 'hello foo';
 
-				const name = 'foo';
+            const actualGreeting = (function (nameParameter) {
+               return ('hello ' + nameParameter);
+            }(name));
 
-				const expectedGreeting = 'hello foo';
+            expect(actualGreeting) .toStrictEqual(expectedGreeting);
 
-				const actualGreeting = ( function (nameParameter) { return ('hello ' + nameParameter); } (name));
-
-				expect(
-					( function (nameParameter)  { return ('hello ' + nameParameter); } (name))
-					)
-					.toStrictEqual(expectedGreeting);
-
-			});
+         });
 
 
-
-		});
-
-
-	});
-
-	describe('Map features', function() {
-
-		test('map applies an operator on each element', () => {
-
-			const anArray     =  [1, 2, 3];
-			const squaredArray = [1, 4, 9];
-
-			const mappedArray =  anArray.map(function(elem){ return elem * elem });
-
-			expect( mappedArray).toStrictEqual(squaredArray);
-
-		});
+      });
 
 
-	});
+   });
 
-	describe('Reduce features', function() {
+   describe('Map features', function () {
+
+      test('map applies an operator on each element', () => {
+
+         const anArray = [1, 2, 3];
+         const squaredArray = [1, 4, 9];
+
+         const mappedArray = anArray.map(function (elem) {
+            return elem * elem
+         });
+
+         expect(mappedArray).toStrictEqual(squaredArray);
+
+      });
+
+      test('map on async returns Promise', async () => {
+
+         const anArray = [1, 2, 3];
+         const squaredArray = [1, 4, 9];
+
+         const mappedArray = await Promise.all(
+            anArray.map((elem) => {
+                  return new Promise((resolve) => {
+                     resolve(elem * elem);
+                  })
+               }
+            )
+         );
+
+         expect(mappedArray).toStrictEqual(squaredArray);
+
+      });
 
 
-		test('reduce applies an operator on all elements ', () => {
 
-			const anArray    = [1,  2,  3];
-			const arrayTotal =  1 + 2 + 3;
 
-			expect(
-				anArray.reduce(function(total, elem){ return elem + total }, 0)
-			).toBe(arrayTotal);
+   });
 
-		});
+   describe('Reduce features', function () {
 
-	});
 
-	describe('Filter features', function() {
+      test('reduce applies an operator on all elements ', () => {
 
-		test('filter keep all elements matching a condition ', () => {
+         const anArray = [1, 2, 3];
+         const arrayTotal = 1 + 2 + 3;
 
-			const anArray             = [1, 2, 3, 4];
-			const arrayWithEvenNumber = [   2,    4];
+         expect(
+            anArray.reduce(function (total, elem) {
+               return elem + total
+            }, 0)
+         ).toBe(arrayTotal);
 
-			const filteredArray =  anArray.filter(function(elem){return elem % 2 == 0; });
+      });
 
-			expect(filteredArray).toStrictEqual(arrayWithEvenNumber);
+   });
 
-		});
+   describe('Filter features', function () {
 
       test('filter keep all elements matching a condition ', () => {
 
-         const anArray             = {1, 2, 3, 4};
-         const arrayWithEvenNumber = [   2,    4];
+         const anArray = [1, 2, 3, 4];
+         const arrayWithEvenNumber = [2, 4];
 
-         const filteredArray =  anArray.filter(function(elem){return elem % 2 == 0; });
+         const filteredArray = anArray.filter(function (elem) {
+            return elem % 2 === 0;
+         });
+
+         expect(filteredArray).toStrictEqual(arrayWithEvenNumber);
+
+      });
+
+      test('filter keep all elements matching a condition ', () => {
+
+         const anArray = [1, 2, 3, 4];
+         const arrayWithEvenNumber = [2, 4];
+
+         const filteredArray = anArray.filter(function (elem) {
+            return elem % 2 === 0;
+         });
 
          expect(filteredArray).toStrictEqual(arrayWithEvenNumber);
 
@@ -150,67 +173,90 @@ describe('Functional tests ', function() {
 
    });
 
-	describe('Closure features', function() {
+   describe('Closure features', function () {
 
-		test('Closure store environnement ', () => {
+      test('Closure store environnement ', () => {
 
-			function createMultiplier(x){
-				return function (y){ return x*y; }
-			}
+         function createMultiplier(x) {
+            return function (y) {
+               return x * y;
+            }
+         }
 
-			const multiplier = 2;
-			const multiplied = 3;
+         const multiplier = 2;
+         const multiplied = 3;
 
-			const multiplierByTwo = createMultiplier (multiplier);
+         const multiplierByTwo = createMultiplier(multiplier);
 
-			const expectedResult = multiplier * multiplied;
-			const actualResult = multiplierByTwo(multiplied);
+         const expectedResult = multiplier * multiplied;
+         const actualResult = multiplierByTwo(multiplied);
 
-			expect(actualResult).toStrictEqual(expectedResult);
+         expect(actualResult).toStrictEqual(expectedResult);
 
-		});
+      });
 
-		test('Closure (fat arrow form) ', () => {
+      test('Closure (fat arrow form) ', () => {
 
-			const createMultiplier = x => y => x * y ;
+         const createMultiplier = x => y => x * y;
 
-			const multiplier = 2;
-			const multiplied = 3;
+         const multiplier = 2;
+         const multiplied = 3;
 
-			const multiplierByTwo = createMultiplier (multiplier);
+         const multiplierByTwo = createMultiplier(multiplier);
 
-			const expectedResult = multiplier * multiplied;
-			const actualResult = multiplierByTwo(multiplied);
+         const expectedResult = multiplier * multiplied;
+         const actualResult = multiplierByTwo(multiplied);
 
-			expect(actualResult).toStrictEqual(expectedResult);
+         expect(actualResult).toStrictEqual(expectedResult);
 
-		});
+      });
 
-		test('Closure (object alternative) ', () => {
+      test('Closure (object alternative) ', () => {
 
-const multiplierObject = {
+         const multiplierObject = {
 
-	multiplier : 0,
+            multiplier: 0,
 
-	multiply : function (multiplied){
-		return(this.multiplier * multiplied);
-	}
-}
+            multiply: function (multiplied) {
+               return (this.multiplier * multiplied);
+            }
+         }
 
-const multiplier = 2
-multiplierObject.multiplier = multiplier;
+         const multiplier = 2
+         multiplierObject.multiplier = multiplier;
 
-const multiplied = 3;
+         const multiplied = 3;
 
-const expectedResult = multiplier * multiplied;
+         const expectedResult = multiplier * multiplied;
 
-const actualResult = multiplierObject.multiply(multiplied);
+         const actualResult = multiplierObject.multiply(multiplied);
 
-expect(actualResult).toStrictEqual(expectedResult);
+         expect(actualResult).toStrictEqual(expectedResult);
 
-		});
+      });
 
+      test('Nested closure is still a closure', async () => {
 
-	});
+         const people = [
+            { name: 'John', friends : ['Tina', 'Paul']},
+            { name: 'Tina', friends : ['John', 'Beth']}
+         ];
+
+         const expectedIntroduction =[
+            ["John is a friend of Tina",  "John is a friend of Paul"],
+            [ "Tina is a friend of John", "Tina is a friend of Beth"]
+         ];
+
+         const introduction = people.map((person) => {
+            return person.friends.map((friend) => {
+               return `${person.name} is a friend of ${friend}`
+            })
+         });
+
+         expect(introduction).toStrictEqual(expectedIntroduction);
+
+      });
+
+   });
 
 });
