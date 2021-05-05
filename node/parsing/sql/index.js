57cb820d256055033b58c4b9cd1a9be5ebe7987f
GradedJestRisk <graded.jest.risk@gmail.com>
2021-05-05 09:53:22 +0200

    [node][parsing][sql] add invalid syntax

--- node/parsing/sql/index.js
+++ node/parsing/sql/index.js
@@ -1,23 +1,31 @@
+const removeNewline = require('newline-remove');
 const {Parser} = require('node-sql-parser');
 
 const parser = new Parser();
-let query;
+const queries = [
+   'SELECT * FROM foo',
+   'SELECT t.id FROM (SELECT id FROM foo)',
+   'INSERT INTO foo (id) SELECT * FROM bar',
+   'INSERT INTO foo (id)      \n             SELECT * FROM bar',
+   'SELECT pg_sleep(5)',
+   'INSERT INTO foo (id) SELECT 1 FROM bar(1)'
+];
 
-const insertSelectQuery = 'INSERT INTO foo (id) SELECT * FROM bar'
-query = insertSelectQuery;
-try {
-   const queryAST = parser.astify(query);
-   console.dir(queryAST);
-} catch (error) {
-   console.log(`error while parsing ${query}`)
-}
+const normalizedQueries = queries.map((query) => {
+   return removeNewline(query)
+})
 
-const spacedQuery = 'INSERT INTO foo (id)                   SELECT * FROM bar';
-const trimmed = spacedQuery.replace(/ +(?= )/g,'');
-query = trimmed;
-try {
-   const queryAST = parser.astify(query);
-   console.dir(queryAST);
-} catch (error) {
-   console.log(`error while parsing ${query}`)
-}
+normalizedQueries.map((query) => {
+   try {
+      const queryAST = parser.astify(query);
+      console.log(`Query type is ${queryAST.type}`)
+      console.dir(queryAST);
+   } catch (error) {
+      console.log('Query type is unknown');
+      console.log(query);
+   }
+});
+
+
+
+
