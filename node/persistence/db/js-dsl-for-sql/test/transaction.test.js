10978cc21a71eaef7cda995844e5532ada5b87f4
GradedJestRisk <graded.jest.risk@gmail.com>
2021-05-31 18:19:23 +0200

    [node][persistence][db][js-dsl-sql] add isolation level

--- node/persistence/db/js-dsl-for-sql/test/transaction.test.js
+++ node/persistence/db/js-dsl-for-sql/test/transaction.test.js
@@ -37,17 +37,78 @@ describe('transaction', async () => {
 
       it('when rollback is called', async () => {
 
+         const trx = await knex.transaction()
+         let result
+
          try {
-            await knex.transaction(async trx => {
-               await trx(tableName).insert(recipe)
-               await trx.rollback()
-            })
+            await knex(tableName).insert(recipe).transacting(trx)
+            await trx.rollback()
          } catch (error) {
-            //console.error('Error raised:' + error)
+            console.error('Error raised:' + error)
          }
+
+         // true when either committed or rolled back
+         trx.isCompleted().should.be.true
 
          const count = await recipeRepository.count()
          count.should.equal(0)
+      })
+
+   })
+
+   describe('isolation levels', async () => {
+
+      const isolationLevelsPG = {
+         // https://www.postgresql.org/docs/current/transaction-iso.html
+         // "PostgreSQL's Read Uncommitted mode behaves like Read Committed."
+         'READ_UNCOMMITED': 'read committed',
+         'READ_COMMITED': 'read committed',
+         'REPEATABLE_READ': 'repeatable read',
+         'SERIALIZABLE': 'serializable'
+      }
+
+      it('REPEATABLE_READ prevents phantom read', async () => {
+
+         // phantom read (https://www.postgresql.org/docs/current/transaction-iso.html)
+         // A transaction re-executes a query returning a set of rows that satisfy a search condition
+         // and finds that the set of rows satisfying the condition has changed due to another recently-committed transaction.
+         const trx1 = await knex.transaction()
+
+         const trx2 = await knex.transaction({
+            isolationLevel: isolationLevelsPG.REPEATABLE_READ
+         })
+
+         try {
+            const before = (await trx2(tableName).select().count())[0].count
+            await trx1(tableName).insert(recipe)
+            const after = (await trx2(tableName).select().count())[0].count
+            after.should.equal(before).should.equal(0);
+         } catch (error) {
+            //console.error("Error raised:" + error);
+         }
+
+         await recipeRepository.removeAll()
+
+         const trx3 = await knex.transaction()
+
+         const trx4 = await knex.transaction({
+            isolationLevel: isolationLevelsPG.READ_COMMITED
+         })
+
+         try {
+            await knex(tableName).insert(recipe)
+            const before = parseInt((await trx4(tableName).select().count())[0].count);
+            await trx3(tableName).del()
+            const afterTrx3 = parseInt((await trx3(tableName).select().count())[0].count);
+            await trx3.commit()
+            const afterTrx4 = parseInt((await trx4(tableName).select().count())[0].count);
+            before.should.equal(1)
+            afterTrx3.should.equal(0)
+            afterTrx4.should.equal(0)
+         }
+         catch (error) {
+            console.error("Error raised:" + error);
+         }
 
       })
 
@@ -126,8 +187,8 @@ describe('transaction', async () => {
          })
       }
 
-      const MAX_ITERATIONS = 200;
-      let currentIterationCount;
+      const MAX_ITERATIONS = 200
+      let currentIterationCount
 
       const begin = Date.now()
       currentIterationCount = 0
@@ -146,10 +207,10 @@ describe('transaction', async () => {
       // console.log('With transaction  (ms): ' + durationTransaction)
       // console.log('Without transaction (ms): ' + duration)
 
-      const quickerPercent = (durationTransaction - duration) / duration * 100;
-      durationTransaction.should.be.greaterThan(duration);
-      quickerPercent.should.be.greaterThan(20);
-      quickerPercent.should.be.below(100);
+      const quickerPercent = (durationTransaction - duration) / duration * 100
+      durationTransaction.should.be.greaterThan(duration)
+      quickerPercent.should.be.greaterThan(20)
+      quickerPercent.should.be.below(100)
    })
 
 })
