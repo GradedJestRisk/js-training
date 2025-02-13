2eb3c9a9630c6fd8202fd28c2a004092867d9217
GradedJestRisk <graded.jest.risk@gmail.com>
2025-02-13 10:30:54 +0100

    [node][persistence][concurrency] add logging

--- node/persistence/db/client/concurrency/pool.js
+++ node/persistence/db/client/concurrency/pool.js
@@ -2,6 +2,8 @@
 import pg from 'pg';
 const { Pool } = pg;
 
+const maxConnexions = process.env.MAX_CONNECTIONS;
+
 const pool = new Pool({
    host: 'localhost',
    port: process.env.POSTGRESQL_EXPOSED_PORT,
@@ -9,21 +11,28 @@ const pool = new Pool({
    user: process.env.POSTGRESQL_USER_NAME,
    password: process.env.POSTGRESQL_USER_PASSWORD,
    application_name: 'concurrency',
-   max: process.env.MAX_CONNECTIONS,
+   max: maxConnexions,
    idleTimeoutMillis: 30000,
    connectionTimeoutMillis: 0,
 });
 
-// await pool.query('SELECT pg_sleep(1)')
-// await pool.query('SELECT pg_sleep(2)')
-// await pool.query('SELECT pg_sleep(3)')
-// await pool.query('SELECT pg_sleep(4)')
+console.log(`Pool has ${maxConnexions} connection(s) available`);
 
+console.log('submitting queries..');
+
+// Serial
+await pool.query('SELECT pg_sleep(1)');
+await pool.query('SELECT pg_sleep(2)');
+await pool.query('SELECT pg_sleep(3)');
+await pool.query('SELECT pg_sleep(4)');
+
+// Parallel or Serial
 await Promise.all([
    pool.query('SELECT pg_sleep(2)'),
    pool.query('SELECT pg_sleep(4)'),
    pool.query('SELECT pg_sleep(6)'),
 ]);
 
-console.log('all queries executed !');
+console.log('queries executed');
+
 await pool.end();
